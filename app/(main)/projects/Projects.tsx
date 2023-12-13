a52ef67100c4e2c78a6843941dcb55e8efd80901
cczywyc <274230928@qq.com>
2023-12-13 23:56:19 +0800

    修复空数据报错

--- app/(main)/projects/Projects.tsx
+++ app/(main)/projects/Projects.tsx
@@ -4,14 +4,14 @@ import { getSettings } from '~/sanity/queries'
 export async function Projects() {
   const { projects } = await getSettings()
 
-  return (
-    <ul
-      role="list"
-      className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
-    >
-      {projects.map((project) => (
-        <ProjectCard project={project} key={project._id} />
-      ))}
-    </ul>
+  return (projects ?
+          <ul
+              role="list"
+              className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
+          >
+            {projects.map((project) => (
+                <ProjectCard project={project} key={project._id} />
+            ))}
+          </ul> : <strong>🔥 产品和项目很多，待我慢慢更新！</strong>
   )
 }
